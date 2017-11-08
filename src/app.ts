dc6f4da84c9812908e0e7f6de29004f869e3ded2
Christof <christof@xtof.io>
2017-11-08 07:43:01 -0800

    add secondary header

--- src/app.ts
+++ src/app.ts
@@ -4,14 +4,14 @@ export class App {
   rows: Row[] = [new Row(0), new Row(1), new Row(2)];
  
   groups: ColumnGroup[] = [
-    new ColumnGroup("Calendar", "orange", [
+    new ColumnGroup("Calendar", "#fde1ad", [
       new Column("Year", "", [2017, 2018, 2019]),
       new Column("Age", "", [50, 51, 52])]),
-    new ColumnGroup("Income", "lightblue", [
+    new ColumnGroup("Income", "#dcf6ff", [
       new Column("Paycheck","3%", [1, 2, 3]),
       new Column("Bonus", "3%", [1, 2, 3]),
       new Column("Rent", "", [1, 2, 3])]),
-    new ColumnGroup("Expenses", "yellow", [
+    new ColumnGroup("Expenses", "#ffffcb", [
         new Column("household","",  [1, 2, 3]),
         new Column("401k", "", [1, -200, 3]),
         new Column("contrib", "", [1, 2, 3])]),
