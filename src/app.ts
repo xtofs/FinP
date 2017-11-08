d800e503584765e97812cfbe864f37db2e6ebc03
Christof <christof@xtof.io>
2017-11-07 21:58:57 -0800

    added column groups and spacers

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,21 @@
 export class App {
-  message = 'Hello World!';
-  rows: Row[] = [ new Row(0), new Row(1), new Row(2) ];
-  cols: Column[] = [ 
-    new Column("Year",[2017,2018,2019]), 
-    new Column("Age",[50,51,52]), 
-    new Column("Paycheck",[1,2,3]), ]
+  
+
+  rows: Row[] = [new Row(0), new Row(1), new Row(2)];
+ 
+  groups: ColumnGroup[] = [
+    new ColumnGroup("Calendar", "orange", [
+      new Column("Year", "", [2017, 2018, 2019]),
+      new Column("Age", "", [50, 51, 52])]),
+    new ColumnGroup("Income", "lightblue", [
+      new Column("Paycheck","3%", [1, 2, 3]),
+      new Column("Bonus", "3%", [1, 2, 3]),
+      new Column("Rent", "", [1, 2, 3])]),
+    new ColumnGroup("Expenses", "yellow", [
+        new Column("household","",  [1, 2, 3]),
+        new Column("401k", "", [1, -200, 3]),
+        new Column("contrib", "", [1, 2, 3])]),
+  ];
 }
 
 export class Row {
@@ -13,9 +24,14 @@ export class Row {
 }
 
 
+export class ColumnGroup {
+  constructor(readonly name: string, readonly color: string, readonly columns: Column[]) {
+  }
+}
+
 export class Column {
-  constructor(readonly name: string, readonly values: number[]) {
+  constructor(readonly name: string, readonly info:string, readonly values: number[]) {
   }
 
-  
+
 }
