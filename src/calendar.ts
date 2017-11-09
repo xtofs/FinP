ac9b43259a1a12f4de3924bbebc0a85994402a5c
Christof <christof@xtof.io>
2017-11-08 21:59:33 -0800

    more table formatting

--- src/calendar.ts
+++ src/calendar.ts
@@ -8,14 +8,14 @@ export class Calendar {
 
     public rows: Row[] = [new Row(0), new Row(1), new Row(2)];
     public groups: ColumnGroup[] = [
-        new ColumnGroup("Calendar", "#fde1ad", [
+        new ColumnGroup("T", "#fde1ad", [
             new Column("Year", "", [2017, 2018, 2019], true),
             new Column("Age", "", [50, 51, 52])]),
         new ColumnGroup("Income", "#dcf6ff", [
             new Column("Paycheck", "3%", [1, 2, 3]),
             new Column("Bonus", "3%", [1, 2, 3]),
             new Column("Rent", "", [1, 2, 3]),
-            new Column("total income", "", [1, 2, 3], true)
+            new Column("Total", "", [1, 2, 3], true)
         ]),
         new ColumnGroup("Expenses", "#ffffcb", [
             new Column("household", "", [1, 2, 3]),
@@ -25,22 +25,22 @@ export class Calendar {
             new Column("stock ctl", "", [1, -200, 3]),
             new Column("401k", "", [1, -200, 3]),
             new Column("contrib", "", [1, -200, 3]),
-            new Column("total expense", "", [1, 2, 3], true)
+            new Column("Total", "", [1, 2, 3], true)
         ]),
         new ColumnGroup("Investments", "#cfffe6", [
-            new Column("start of year", "", [1, 2, 3], true),
+            new Column("SoY", "", [1, 2, 3], true),
             new Column("401k", "", [1, -200, 3]),
             new Column("espp", "", [1, -200, 3]),
             new Column("401k ctl", "", [1, -200, 3]),
             new Column("stock ctl", "", [1, -200, 3]),
             new Column("401k", "", [1, -200, 3]),
             new Column("contrib", "", [1, -200, 3]),
-            new Column("end of year", "", [1, 2, 3], true)
+            new Column("EoY", "", [1, 2, 3], true)
         ]),
     ];
 
-    public collapse(x){
-        x.visible=!x.visible;
+    public collapse(x: ColumnGroup){
+        x.expanded = !x.expanded;
     }
 }
 
@@ -55,10 +55,10 @@ export class ColumnGroup {
     constructor(readonly name: string, readonly color: string, readonly columns: Column[]) {
     }
 
-    public visible: boolean = true;
+    public expanded: boolean = true;
 
     public get colspan(): number {
-        return  this.visible ? this.columns.length : this.columns.filter(c => c.summary).length;
+        return  this.expanded ? this.columns.length : this.columns.filter(c => c.summary).length;
     }
 }
 
